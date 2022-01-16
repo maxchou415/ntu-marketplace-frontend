f2eee18e4b86f32b22a0f3d105bffb9dfac1a476
Max Chou <yihsunmaxchou@icloud.com>
2022-01-17 07:40:21 +0800

    Add footer

---
+++ components/Index/footer.vue
@@ -0,0 +1,9 @@
+<template lang="pug">
+  .px-4.pt-16.mx-auto
+    .flex.flex-col-reverse.justify-between.pt-5.pb-10.border-t(class='lg:flex-row')
+      p.font-semibold.text-gray-400.font-sans.uppercase.tracking-wide
+        | &copy; Copyright 2022 Team
+        span.text-indigo-500.ml-1 MyNTU
+        |. All rights reserved.
+  
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,19 @@
   div
     hero
     services
+    customized-footer
 </template>
 
 <script>
 import hero from '@/components/Index/hero'
 import services from '@/components/Index/services'
+import footer from '@/components/Index/footer'
 
 export default {
   components: {
     hero,
-    services
+    services,
+    customizedFooter: footer
   }
 }
 </script>
